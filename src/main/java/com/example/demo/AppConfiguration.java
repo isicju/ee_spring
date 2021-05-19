52e9de73c2f70a5c75573ac6fff63df70012e265
Dmitry Egorov <isicju@gmail.com>
2021-05-19 20:26:09 +0100

    ui test

--- src/main/java/com/example/demo/AppConfiguration.java
+++ src/main/java/com/example/demo/AppConfiguration.java
@@ -32,7 +32,7 @@ public class AppConfiguration  {
     public DataSource embeddedDataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1;MODE=MYSQL");
+        dataSource.setUrl("jdbc:h2:mem:hr;DB_CLOSE_DELAY=-1;MODE=MYSQL");
         dataSource.setUsername("sa");
         dataSource.setPassword("sa");
 
