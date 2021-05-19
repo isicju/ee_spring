37700d32fed2a5f42acc80eb1449d9ebfcb4e0dd
Dmitry Egorov <isicju@gmail.com>
2021-05-19 14:54:40 +0100

    Spring based app with draft ui test

--- src/main/java/com/example/demo/AppConfiguration.java
+++ src/main/java/com/example/demo/AppConfiguration.java
@@ -1,40 +1,35 @@
 package com.example.demo;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import lombok.Builder;
-import lombok.Data;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-public class AppConfiguration extends WebMvcConfigurerAdapter {
+public class AppConfiguration  {
 
     @Profile("prod")
-    @Bean(name = "mySqlDataSource")
-    public DataSource service() {
+    @Bean
+    public DataSource prodDataSource() {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setPassword("secret_password");
         dataSource.setUser("root");
-        dataSource.setURL("jdbc:mysql://185.204.3.211:3306/hr?useUnicode=true&serverTimezone=UTC");
+        dataSource.setURL("jdbc:mysql://51.124.98.77:3306/hr?useUnicode=true&serverTimezone=UTC");
         return dataSource;
     }
 
     @Profile("test")
     @Bean("h2DataSouce")
-    public DataSource dataSourceTest(){
+    public DataSource embeddedDataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1;MODE=MYSQL");
@@ -48,7 +43,7 @@ public class AppConfiguration extends WebMvcConfigurerAdapter {
 
         return dataSource;
     }
-    @Profile({"test","prod"})
+
     @Bean()
     public Properties emailProperties(){
         Properties properties = new Properties();
