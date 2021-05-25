1eaff5f0a01811c223e4fa1c47a60c0548255393
Dmitry Egorov <isicju@gmail.com>
2021-05-25 23:30:07 +0100

    jenkins build

--- src/main/java/com/example/demo/AppConfiguration.java
+++ src/main/java/com/example/demo/AppConfiguration.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
@@ -15,9 +15,10 @@ import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.util.Properties;
 
+import static org.apache.logging.log4j.util.Strings.isEmpty;
+@Log4j2
 @Configuration
 @PropertySource("classpath:application.properties")
 public class AppConfiguration {
@@ -37,6 +38,7 @@ public class AppConfiguration {
     @Profile("prod")
     @Bean
     public DataSource prodDataSource(Properties applicationProperties) {
+        log.info("initializing datasource prod database...");
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setPassword(applicationProperties.getProperty("database_password"));
         dataSource.setUser(applicationProperties.getProperty("database_user"));
@@ -44,10 +46,29 @@ public class AppConfiguration {
         return dataSource;
     }
 
+    @Profile("uat")
+    @Bean
+    public DataSource uatDataSource() {
+        log.info("initializing datasource uat database...");
+        String password = System.getProperty("database_password");
+        String user = System.getProperty("database_user");
+        String ip = System.getProperty("database_ip");
+        String port = System.getProperty("database_port");
+
+        if (isEmpty(password) || isEmpty(user) || isEmpty(ip) || isEmpty(port)) {
+            throw new RuntimeException("password, user, ip, port has to be passed as ENVIRONMENT VARIABLE!");
+        }
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setPassword(password);
+        dataSource.setUser(user);
+        dataSource.setURL("jdbc:mysql://" + ip + ":" + port + "/?useUnicode=true&serverTimezone=UTC");
+        return dataSource;
+    }
+
     @Profile("test")
-    @Bean("h2DataSouce")
     public DataSource embeddedDataSource() {
-        System.out.println("initializing database...");;
+        log.info("initializing datasource h2 database...");
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:hr;DB_CLOSE_DELAY=-1;MODE=MYSQL;DATABASE_TO_UPPER=false");
@@ -62,9 +83,9 @@ public class AppConfiguration {
         return dataSource;
     }
 
-    @Profile({"test", "prod"})
+    @Profile({"test", "prod","uat"})
     @Bean("emailProperties")
-    public Properties emailProperties(Properties applicationProperties) throws IOException {
+    public Properties emailProperties(Properties applicationProperties) {
         Properties properties = new Properties();
 
         properties.put("mail.smtp.host", applicationProperties.getProperty("mail.smtp.host"));
